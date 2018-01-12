23a1968872ba739274402b63c06a43ef8e2dbf96
狩野惇 <ash.surfing.life@gmail.com>
2018-01-13 01:22:21 +0900

    1/13 フラッシュ表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
 
+
+
+# API = "http://webservice.recruit.co.jp/hotpepper/service_area/v1/?key=6cca7f57da84355c"
+#
+# xml = Nokogiri::XML(open(API).read)
+#
+# url_nodes = xml.xpath("//url").text
+#
+# puts url_nodes
 
   def top
+    url = 'http://webservice.recruit.co.jp/hotpepper/service_area/v1/?key=6cca7f57da84355c'
+
+    @xmls = Nokogiri::XML(open(url).read)
+
   end
 
   def about
