d3d8df93b0d56a73d3deb0f8bacaa0cca4629f42
狩野惇 <ash.surfing.life@gmail.com>
2018-01-21 01:31:28 +0900

    ユーザーと投稿紐付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
   get "signup" => "users#new"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
-  get "users/:id/edit" => "users#edit"
-  post "users/:id/update" => "users#update"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
@@ -14,7 +16,6 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
-  get "posts/:id/time" => "posts#time"
 
   get "/" => "home#top"
   get "about" => "home#about"
